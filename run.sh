6914b071e4040cb3ad6eaa928a734573794bec7e
Warrick Ball <warrickball@gmail.com>
2024-06-09 21:40:18 +0100

    iterate on 'cat */test_suite/*/inlist_pgstar'

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sed -E \
-    -e 's/(History_Track|Grid|Profile_Panels)([1-9])([_a-zA-Z]*) = /\1\3\(\2\) = /' \
-    -e 's/(History_Track|Grid|Profile_Panels)([1-9])([_a-zA-Z]*)\(/\1\3\(\2\,/' \
+    -e 's/(History_Track|Grid|Profile_Panels|History_Panels|Text_Summary)([1-9])([_a-zA-Z]*) = /\1\3\(\2\) = /' \
+    -e 's/(History_Track|Grid|Profile_Panels|History_Panels|Text_Summary)([1-9])([_a-zA-Z]*)\(/\1\3\(\2\,/' \
     "${1}" > "${1}".modified
-diff -yw "${1}".modified "${2}" && echo "OK" || echo "Differences remain!"
+diff --suppress-common-lines -yw "${1}".modified "${2}" && echo "OK" || echo "Differences remain!"
