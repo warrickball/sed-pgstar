723828ef602c38f0a1e56a5de7acf4f4f4c1f0aa
Warrick Ball <warrickball@gmail.com>
2024-06-09 21:26:48 +0100

    seems to catch most 2d arrays too

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-sed \
-    -E '/(History_Track|Grid|Profile_Panels)/s/([1-9])([_a-zA-Z]*) = /\2\(\1\) = /' \
+sed -E \
+    -e 's/(History_Track|Grid|Profile_Panels)([1-9])([_a-zA-Z]*) = /\1\3\(\2\) = /' \
+    -e 's/(History_Track|Grid|Profile_Panels)([1-9])([_a-zA-Z]*)\(/\1\3\(\2\,/' \
     "${1}" > "${1}".modified
 diff -yw "${1}".modified "${2}" && echo "OK" || echo "Differences remain!"
